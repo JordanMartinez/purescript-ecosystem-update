de0c97b4bd1a81a14f07f66b13f11216c4bd489b
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-10-12 12:49:29 -0700

    Finish updating script

--- setupRemote.sh
+++ setupRemote.sh
@@ -17,35 +17,42 @@ PS_TAG=v0.14.0-rc3
 # go up one level
 cd ..
 
-# Fork the repo using GitHub's CLI tool, but don't `git clone` it here
+# Fork the repo using GitHub's CLI tool and `git clone` it.
 gh repo fork $BASE_GH_ORG/purescript-$1 --clone=true --remote=true
 
+# change into that just cloned repo
 cd purescript-$1
 
 # Checkout a new branch based on the current `master` branch
 git checkout -b updateTo14
 
+# Ensure we're at the real repo's current `master` branch in case
+# we've done work on this repo before in our own fork
 git reset --hard upstream/master
 
-# use 'sed' to update dependencies to `master`
+# use 'sed' to update all purescript dependencies in bower to `master`
 echo "Updating all deps in `bower.json` to `master`"
 sed --in-place -r 's/\^[0-9]+\.[0-9]+\.[0-9]+/master/g' bower.json
 
-# use `sed` to update psa to v0.8.0
+# use `sed` to update purescript-psa to v0.8.0
 echo "Updating `purescript-psa` to `v0.8.0`"
 sed --in-place 's/"purescript-psa": "^0.6.0"/"purescript-psa": "^0.8.0"/' package.json
 
 # use `sed` to update TAG in .travis.yml to latest release candidate
+# by commenting out old one
+# and inserting new one in front of the next command (i.e. `curl`)
 echo "Updating `.travis.yml` TAG to $PS_TAG"
-## comment out the previous TAG
-sed --in-place 's/- TAG/# - TAG/g' .travis.yml
-## insert `  - TAG=PS_TAG` and a newline in front of the `  - curl` line
+sed --in-place 's/- TAG/# - TAG/' .travis.yml
+
 ## See https://stackoverflow.com/a/584926
 sed --in-place 's/- curl/- TAG='"$PS_TAG"'\n  - curl/' .travis.yml
 
+# Add these files and commit them to our branch
 git add bower.json package.json .travis.yml
 git commit -m "Update TAG to $PS_TAG; dependencies to master; psa to v0.8.0"
 
+echo ""
+echo "Try running `./compile.sh $1` and use `1` to select the `master` branch if `bower` complains"
 echo ""
 echo "Open the below URL to see whether repo has any pre-existing PRs and/or issues"
 echo https://github.com/purescript/purescript-$1
