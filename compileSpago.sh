dfec646f7624e012f657d2690c1cbaac225efcc7
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-12-02 18:37:04 -0800

    Add verbose flag

--- compileSpago.sh
+++ compileSpago.sh
@@ -10,5 +10,5 @@ set -euo pipefail
 # `purescript-contrib/purescript-now` repo
 export PATH="$(pwd):$PATH"
 cd ../purescript-$1
-spago build -u "--strict"
+spago --verbose build -u "--strict"
 spago test
