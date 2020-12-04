4ede7e34467c4f029dac7e54b806f266a33090e5
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-12-03 19:36:20 -0800

    Also check for unsafeCoerce usages

--- usageOfProxy.sh
+++ usageOfProxy.sh
@@ -6,5 +6,7 @@ cd ../purescript-$1
 
 grep -r --include "*.purs" Proxy src/
 grep -r --include "*.purs" Global src/
+grep -r --include "*.purs" unsafeCoerce src/
 grep -r --include "*.purs" Proxy test/
 grep -r --include "*.purs" Global test/
+grep -r --include "*.purs" unsafeCoerce test/
