64e5332df616f6958fa6774c1dbde6373536ed42
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-12-08 06:40:50 -0800

    Make it easier to backlink to different issues

--- pr.sh
+++ pr.sh
@@ -9,8 +9,11 @@ set -euo pipefail
 # cd into the correct directory
 cd ../purescript-$1
 
+ISSUE=purescript/purescript\#3492
+#ISSUE=purescript-contrib/governance\#35
+
 # Use gh to create the PR
-gh pr create --title "Update to v0.14.0-rc3" --body "Backlinking to purescript-contrib/governance#35"
+gh pr create --title "Update to v0.14.0-rc3" --body "Backlinking to $ISSUE"
 # 1st question: Choose the `purescript/purescript-<packageName>`
 # 2nd question: Choose your repo
 # 3rd Question: Choose 'Submit'
