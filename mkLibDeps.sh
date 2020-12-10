1144bb7fce194445ad74b9a1b95b332cfd4ac184
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-12-10 06:29:55 -0800

    Add node specific packages deps list; add original package desp list

--- mkLibDeps.sh
+++ mkLibDeps.sh
@@ -10,8 +10,11 @@ set -euo pipefail
 # Produces the `libDeps.txt` file
 node ./package-query.js --input ./packageSet.json --force genLibDeps --output ./libDeps.txt --finished-dependencies-file ./finished-dependencies.txt
 
+node ./package-query.js --input ./packageSet.json --force genLibDeps --output ./originalPackagesList.txt
+
 # Sleep for 0.1 seconds. otherwise only one file will be outputted
 sleep 0.1
 # cat ./libDeps.txt | grep '/purescript/' > ./libDeps-purescript.txt
 cat ./libDeps.txt | grep '/purescript-contrib/' > ./libDeps-purescript-contrib.txt
 cat ./libDeps.txt | grep '/purescript-web/' > ./libDeps-purescript-web.txt
+cat ./libDeps.txt | grep '/purescript-node/' > ./libDeps-purescript-node.txt
