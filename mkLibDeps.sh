0800c68428bdf04f4e998368f034483a6ef61a50
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-01 11:02:33 -0600

    Update script

--- mkLibDeps.sh
+++ mkLibDeps.sh
@@ -8,13 +8,16 @@ set -euo pipefail
 # on a newline and then rerun this script
 
 # Produces the `libDeps.txt` file
-node ./package-graph.js --input ./packageSet.json --force lib-deps --output ./libDeps.txt --deps ./finished-dependencies.txt
+node ./package-graph.js --input ./packages-0.14.7.json --force lib-deps --output ./libDeps.txt --deps ./finished-dependencies.txt
 
-node ./package-graph.js --input ./packageSet.json --force lib-deps --output ./originalPackagesList.txt
+node ./package-graph.js --input ./packages-0.14.7.json --force lib-deps --output ./originalPackagesList.txt
 
 # Sleep for 0.1 seconds. otherwise only one file will be outputted
 sleep 0.1
 cat ./libDeps.txt | grep '/purescript/' >./libDeps-purescript.txt
+sleep 0.1
 cat ./libDeps.txt | grep '/purescript-contrib/' >./libDeps-purescript-contrib.txt
+sleep 0.1
 cat ./libDeps.txt | grep '/purescript-web/' >./libDeps-purescript-web.txt
+sleep 0.1
 cat ./libDeps.txt | grep '/purescript-node/' >./libDeps-purescript-node.txt
