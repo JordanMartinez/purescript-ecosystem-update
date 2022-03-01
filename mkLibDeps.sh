863d9000de4013efdd4c38c4648e612c5171d30e
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-01 10:56:45 -0600

    Update package-query with latest compiled output

--- mkLibDeps.sh
+++ mkLibDeps.sh
@@ -8,13 +8,13 @@ set -euo pipefail
 # on a newline and then rerun this script
 
 # Produces the `libDeps.txt` file
-node ./package-query.js --input ./packageSet.json --force genLibDeps --output ./libDeps.txt --finished-dependencies-file ./finished-dependencies.txt
+node ./package-graph.js --input ./packageSet.json --force lib-deps --output ./libDeps.txt --deps ./finished-dependencies.txt
 
-node ./package-query.js --input ./packageSet.json --force genLibDeps --output ./originalPackagesList.txt
+node ./package-graph.js --input ./packageSet.json --force lib-deps --output ./originalPackagesList.txt
 
 # Sleep for 0.1 seconds. otherwise only one file will be outputted
 sleep 0.1
-cat ./libDeps.txt | grep '/purescript/' > ./libDeps-purescript.txt
-cat ./libDeps.txt | grep '/purescript-contrib/' > ./libDeps-purescript-contrib.txt
-cat ./libDeps.txt | grep '/purescript-web/' > ./libDeps-purescript-web.txt
-cat ./libDeps.txt | grep '/purescript-node/' > ./libDeps-purescript-node.txt
+cat ./libDeps.txt | grep '/purescript/' >./libDeps-purescript.txt
+cat ./libDeps.txt | grep '/purescript-contrib/' >./libDeps-purescript-contrib.txt
+cat ./libDeps.txt | grep '/purescript-web/' >./libDeps-purescript-web.txt
+cat ./libDeps.txt | grep '/purescript-node/' >./libDeps-purescript-node.txt
--- rebuildRemainingPackages.sh
+++ rebuildRemainingPackages.sh
@@ -9,10 +9,10 @@ set -euo pipefail
 DEPS=./finished-dependencies.txt
 
 # Overwrite DEPS and add proxy and generics-rep to file
-echo "" > $DEPS
-echo "proxy" >> $DEPS
-echo "generics-rep" >> $DEPS
-echo "globals" >> $DEPS
+echo "" >$DEPS
+echo "proxy" >>$DEPS
+echo "generics-rep" >>$DEPS
+echo "globals" >>$DEPS
 
 # overwrite the `packages-0.14.dhall` file with hash-less copy
 # so that any new updates are automatically pulled in
@@ -20,10 +20,10 @@ cp packages-0.14-no-hash.dhall packages-0.14.dhall
 
 echo "Finding packages that have been updated...."
 # Now use that package set to compute those that have already been updated
-spago -x spago-0.14.dhall ls packages | cut -d ' ' -f 1 >> $DEPS
+spago -x spago-0.14.dhall ls packages | cut -d ' ' -f 1 >>$DEPS
 echo "Done."
 
 echo "Calculating unblocked libraries"
 # Now calculate the ones that are unblocked
-node ./package-query.js --input ./packageSet.json --force genLibDeps --output ./libDeps.txt --finished-dependencies-file $DEPS
+node ./package-graph.js --input ./packageSet.json --force lib-deps --output ./libDeps.txt --deps $DEPS
 echo "Done."
