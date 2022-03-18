5d1efea2ca87b77f9f936658f07627a6e0e63134
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-18 14:31:35 -0500

    Fix libDeps org-specific file issue

--- mkLibDeps.sh
+++ mkLibDeps.sh
@@ -13,11 +13,11 @@ node ./package-graph.js --input ./packages-0.14.7.json --force lib-deps --output
 # node ./package-graph.js --input ./packages-0.14.7.json --force lib-deps --output ./originalPackagesList.txt
 
 # Sleep for 0.1 seconds. otherwise only one file will be outputted
-sleep 0.1
+sleep 0.3
 cat ./libDeps.txt | grep '/purescript/' >./libDeps-purescript.txt
-sleep 0.1
+sleep 0.3
 cat ./libDeps.txt | grep '/purescript-contrib/' >./libDeps-purescript-contrib.txt
-sleep 0.1
+sleep 0.3
 cat ./libDeps.txt | grep '/purescript-web/' >./libDeps-purescript-web.txt
-sleep 0.1
+sleep 0.3
 cat ./libDeps.txt | grep '/purescript-node/' >./libDeps-purescript-node.txt
