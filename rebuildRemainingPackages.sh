ceb4282e470343b68810df1f9153e1a9bd0c6cb3
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-21 12:30:20 -0500

    Fail if script is used

--- rebuildRemainingPackages.sh
+++ rebuildRemainingPackages.sh
@@ -8,6 +8,9 @@ set -euo pipefail
 
 ROOT_DIR=$(dirname "$(readlink -f "$0")")
 
+echo "This script was used in a way I don't remember in the v0.14.0 update. This script needs to be updated"
+exit 1
+
 DEPS="$ROOT_DIR/finished-dependencies.txt"
 
 # Overwrite DEPS and add proxy and generics-rep to file
@@ -27,5 +30,6 @@ echo "Done."
 
 echo "Calculating unblocked libraries"
 # Now calculate the ones that are unblocked
+
 node "$ROOT_DIR/src/node/lib/package-graph.js" --input ./packageSet.json --force lib-deps --output ./libDeps.txt --deps $DEPS
 echo "Done."
