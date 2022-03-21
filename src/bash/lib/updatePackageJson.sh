7a6227df3adb05b5540b3b7029509c360084aa9c
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-21 18:13:09 -0500

    Update pulp and psa separately

--- src/bash/lib/updatePackageJson.sh
+++ src/bash/lib/updatePackageJson.sh
@@ -6,12 +6,21 @@ function updatePackageJson::main {
     jq '
         if has("devDependencies") then
           .devDependencies |= (
-            if has("purescript-psa") then ."purescript-psa" = "^0.8.2" else . end |
             if has("pulp") then ."pulp" = "16.0.0-0" else . end
           )
         else . end
       ' package.json > $TMP_FILE && mv $TMP_FILE package.json
     git add package.json
-    git commit -m "Update pulp to 16.0.0-0 and psa to 0.8.2"
+    git commit -m "Update pulp to 16.0.0-0"
+
+    jq '
+        if has("devDependencies") then
+          .devDependencies |= (
+            if has("purescript-psa") then ."purescript-psa" = "^0.8.2" else . end |
+          )
+        else . end
+      ' package.json > $TMP_FILE && mv $TMP_FILE package.json
+    git add package.json
+    git commit -m "Update psa to 0.8.2"
   fi
 }
