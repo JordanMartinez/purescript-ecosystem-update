7c0855687c7563731be1464cbc627aedd5bce528
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-21 09:55:38 -0500

    Revert to using sed to update ci.yml file

--- src/bash/lib/updateGhActions.sh
+++ src/bash/lib/updateGhActions.sh
@@ -2,38 +2,12 @@
 
 ROOT_DIR=$(dirname "$(readlink -f "$0")")
 
-# Updates the `.github/worksflows/ci.yml` file
-# by checking whether the difference between the
-# current file and the desired one match.
-# (This uses a pre-computed diff).
-# If so, overwrites current one with desired one.
+# Update the `.github/workflows/ci.yml` file to specifically use
+# the latest alpha PS release
 function updateGhActions::main {
-  if [ -f ".github/workflows/ci.yml" ]; then
-    local TEMP_FILE TARGET_FILE
-    TARGET_FILE=ci.yml
-    TEMP_FILE="$TARGET_FILE.new"
-    pushd .github/workflows
-
-    function updateEslint::main::checkAndCommit {
-      local FILE_NUM CONTENT DIFF_EXPECTED DIFF_ACTUAL
-      FILE_NUM="$1"
-      CONTENT=$(cat "$ROOT_DIR/files/gh-actions/$FILE_NUM.yml")
-      DIFF_EXPECTED=$(cat "$ROOT_DIR/files/gh-actions/$FILE_NUM.yml.diff")
-      echo "$CONTENT" > "$TEMP_FILE"
-      DIFF_ACTUAL=$(diff $TARGET_FILE $TEMP_FILE)
-      if [ "$DIFF_EXPECTED" == "$DIFF_ACTUAL" ]; then
-        echo "Match found on $FILE_NUM"
-        mv "$TEMP_FILE" "$TARGET_FILE"
-        git add "$TARGET_FILE"
-        git commit -m "Updated $TARGET_FILE using file pattern: $FILE_NUM"
-      else
-        echo "Match failed for $FILE_NUM"
-      fi
-    }
-
-    updateEslint::main::checkAndCommit "1"
-    popd
-  else
-    echo "No .github/workflows/ci.yml file found. Skipping CI update."
-  fi
+  echo "Update ci.yml to use purescript unstable"
+  sed -i 's/        uses: purescript-contrib\/setup-purescript@main/        uses: purescript-contrib\/setup-purescript@main\n        with:\n          purescript: "unstable"/' .github/workflows/ci.yml
+  sed -i 's/      - uses: purescript-contrib\/setup-purescript@main/      - uses: purescript-contrib\/setup-purescript@main\n        with:\n          purescript: "unstable"/' .github/workflows/ci.yml
+  git add .github/workflows/ci.yml
+  git commit -m "Update to CI to use 'unstable' purescript"
 }
