f8390f09bd5bb8d7906504d9853c31465b6ebda6
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-22 16:29:52 -0500

    Add another eslint config

--- src/bash/lib/updateEslint.sh
+++ src/bash/lib/updateEslint.sh
@@ -53,6 +53,7 @@ function updateEslint::main {
     updateEslint::main::checkAndCommit "5"
     updateEslint::main::checkAndCommit "6"
     updateEslint::main::checkAndCommit "7"
+    updateEslint::main::checkAndCommit "8"
   else
     echo "No .eslintrc.json file found. Skipping eslint update."
   fi
