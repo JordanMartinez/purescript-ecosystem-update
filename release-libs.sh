38016917d138aa0ad61198b498fbbb55d1bccdc6
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-04-04 15:00:58 -0500

    Add updated version of Cyril's script

--- release-libs.sh
+++ release-libs.sh
@@ -5,7 +5,7 @@ echo "Stopping script execution to prevent accidental usage."
 echo "Remove these three two lines locally"
 exit 1
 
-function show {
+function show_changes {
   git --no-pager show --format=''
 }
 
@@ -21,8 +21,8 @@ function update_ci {
   fi
   mv ci.yml.tmp .github/workflows/ci.yml
   git add .github/workflows/ci.yml
-  git commit -m "Use the latest compiler version on CI"
-  show
+  git commit -m "Update CI to build with the latest version of the compiler"
+  show_changes
 }
 
 function update_bower_repository_url {
@@ -35,37 +35,65 @@ function update_bower_repository_url {
     jq --arg repository_url "$repository_url" '.repository.url = $repository_url' bower.json > bower.json.tmp
     mv bower.json.tmp bower.json
     git add bower.json
-    git commit -m "Update the bower repository url to the one known by the registry"
-    show
+    git commit -m "Update the bower repository URL to match the URL in the registry"
+    show_changes
   fi
+}
+
+function update_bower_dependencies_with {
+  local packages="$1"
+  local key="$2"
+  local names="$3"
+  if [ -z "$names" ] || [ "$names" = '[]' ]; then
+    jq --arg key "$key" 'del(.[$key])' bower.json > bower.json.tmp
+  else
+    local versions; versions="$(jq --argjson packages "$packages" 'map(ltrimstr("purescript-") | . as $package_name | $packages[$package_name].version | sub("^v";"^")? // null)' <<< "$names")"
+    local dependencies; dependencies="$(jq --null-input --argjson keys "$names" --argjson values "$versions" '[[$keys, $values] | transpose[] | {key:.[0],value:.[1]}] | from_entries')"
+    jq --arg key "$key" --argjson dependencies "$dependencies" '.[$key] = $dependencies' bower.json > bower.json.tmp
+  fi
+  mv bower.json.tmp bower.json
 }
 
 function update_bower_dependencies {
   echo "Updating bower dependencies"
-  for key in 'dependencies' 'devDependencies'; do
+  local packages="$1"
+  for key in dependencies devDependencies; do
     local names; names="$(jq --arg key "$key" '.[$key] | keys?' bower.json)"
-    [ -n "$names" ] || continue
-    local versions; versions="$(jq --argjson packages "$packages"  'map(ltrimstr("purescript-") | . as $package_name | $packages[$package_name].version | sub("^v";"^")? // null)' <<< "$names")"
-    local dependencies; dependencies="$(jq --null-input --argjson keys "$names" --argjson values "$versions" '[[$keys, $values] | transpose[] | {key:.[0],value:.[1]}] | from_entries')"
-    jq --arg key "$key" --argjson dependencies "$dependencies" '.[$key] = $dependencies' bower.json > bower.json.tmp
-    mv bower.json.tmp bower.json
+    update_bower_dependencies_with "$packages" "$key" "$names"
   done
   if ! git diff --quiet --exit-code bower.json; then
     git add bower.json
-    git commit -m "Update bower dependencies"
-    show
+    git commit -m "Upgrade bower dependencies"
+    show_changes
+  fi
+}
+
+function update_contrib_bower_dependencies {
+  echo "Updating bower dependencies"
+  local packages="$1"
+  local package_name="$2"
+  curl --silent --show-error https://raw.githubusercontent.com/kl0tl/package-sets/prepare-0.14/packages.dhall > ./packages.dhall
+  local dependencies; dependencies="$(jq --arg package_name "$package_name" '.[$package_name].dependencies | map("purescript-" + .)' <<< "$packages")"
+  update_bower_dependencies_with "$packages" dependencies "$dependencies"
+  local dev_dependencies; dev_dependencies="$(dhall-to-json <<< './spago.dhall' | jq --argjson dependencies "$dependencies" '.dependencies - ["psci-support"] | map("purescript-" + .) | . - $dependencies')"
+  update_bower_dependencies_with "$packages" devDependencies "$dev_dependencies"
+  if ! git diff --quiet --exit-code bower.json; then
+    git add bower.json
+    git commit -m "Upgrade bower dependencies"
+    show_changes
   fi
 }
 
 function update_changelog {
   echo "Updating changelog"
-  local package_name="$1"
-  local version="$2"
-  local release_date="$3"
-  sed --in-place -e "s/## \[Unreleased\]/## \[Unreleased\]\\n\\nBreaking changes:\\n\\nNew features:\\n\\nBugfixes:\\n\\nOther improvements:\\n\\n## [$version](https:\/\/github.com\/purescript\/purescript-$package_name\/releases\/tag\/$version) - $release_date/" CHANGELOG.md
+  local org="$1"
+  local package_name="$2"
+  local version="$3"
+  local release_date="$4"
+  sed --in-place -e "s/## \[Unreleased\]/## \[Unreleased\]\\n\\nBreaking changes:\\n\\nNew features:\\n\\nBugfixes:\\n\\nOther improvements:\\n\\n## [$version](https:\/\/github.com\/$org\/purescript-$package_name\/releases\/tag\/$version) - $release_date/" CHANGELOG.md
   git add CHANGELOG.md
-  git commit -m "Update CHANGELOG.md"
-  show
+  git commit -m "Update the changelog"
+  show_changes
 }
 
 function update_prelude_changelog {
@@ -75,30 +103,30 @@ function update_prelude_changelog {
   local release_date="$3"
   sed --in-place -e "s/## \[Unreleased\] - YEAR-MONTH-DATE/## [$version](https:\/\/github.com\/purescript\/purescript-$package_name\/releases\/tag\/$version) - $release_date/" CHANGELOG.md
   git add CHANGELOG.md
-  git commit -m "Update CHANGELOG.md"
-  show
+  git commit -m "Update the changelog"
+  show_changes
 }
 
 function update_contrib_changelog {
   sed --in-place -e 's/ (😱!!!)//' CHANGELOG.md
-  update_changelog "$1" "$2" "$3"
+  update_changelog purescript-contrib "$1" "$2" "$3"
 }
 
-function update_node_library_changelog {
+function update_node_changelog {
   echo "Updating changelog"
   local release_date="$1"
   sed --in-place -e "s/2021-MONTH-DAY/$release_date/" CHANGELOG.md
   git add CHANGELOG.md
-  git commit -m "Update CHANGELOG.md"
-  show
+  git commit -m "Update the changelog"
+  show_changes
 }
 
 function pull_request_body {
-  local repository_url="$1"
-  local branch="$2"
+  local org="$1"
+  local repository_url="$2"
   local version="$3"
-  local release_notes; release_notes="$(sed -n "/## \[$version\]/,/## \[/p" CHANGELOG.md | sed -e '$d')"
-  local new_release_url; new_release_url="$(sed -e 's/\.git$//' <<< "$repository_url")/releases/new?tag=$version&target=$branch&title=$version&body=$(jq --slurp --raw-input --raw-output '@uri' <<< "$release_notes")"
+  local release_notes; release_notes="$(sed -n "/## \[$version\]/,/## \[/p" CHANGELOG.md | sed -e '1d;$d')"
+  local new_release_url; new_release_url="$(sed -e 's/\.git$//' <<< "$repository_url")/releases/new?tag=$version&title=$version&body=$(jq --slurp --raw-input --raw-output '@uri' <<< "$release_notes")"
 
 cat << EOM
 :robot: This is an automated pull request to prepare the next release of this library.
@@ -124,46 +152,51 @@ function open_pull_request {
 function main {
   local org="$1"
   local release_date="$2"
+  local package_set; package_set='https://raw.githubusercontent.com/kl0tl/package-sets/next'
+  local packages; packages="$(dhall-to-json <<< "$package_set/src/groups/purescript.dhall // $package_set/src/groups/purescript-contrib.dhall // $package_set/src/groups/purescript-web.dhall // $package_set/src/groups/purescript-node.dhall")"
+  local bower_packages; bower_packages="$(curl --silent --show-error https://raw.githubusercontent.com/kl0tl/registry/fix-bower-packages/bower-packages.json)"
+  local new_packages; new_packages="$(curl --silent --show-error https://raw.githubusercontent.com/kl0tl/registry/fix-bower-packages/new-packages.json)"
+  local registry; registry="$(jq --null-input --argjson bower_packages "$bower_packages" --argjson new_packages "$new_packages" '$bower_packages + $new_packages')"
   local org_packages; org_packages="$(dhall-to-json <<< "$package_set/src/groups/$org.dhall")"
   for package_name in $(jq --raw-output 'keys | .[]' <<< "$org_packages"); do
-      local package; package="$(jq --arg key "$package_name" '.[$key]' <<< "$org_packages")"
-      local repository_url; repository_url="$(jq --raw-output '.repo' <<< "$package")"
-      local version; version="$(jq --raw-output '.version' <<< "$package")"
-      local branch; branch="release-$version"
-      local workspace; workspace="$(mktemp --directory)"
-      echo "Preparing to release $package_name@$version"
-      echo "Cloning $repository_url"
-      git clone --depth 1 "$repository_url" "$workspace"
-      pushd "$workspace"
-      if git ls-remote --exit-code --heads origin "$branch" > /dev/null; then
-        echo "Release branch $branch found on remote, skipping $package_name"
-        continue
-      fi
-      git checkout -b "$branch"
-      update_ci "$org"
-      update_bower_repository_url "$(jq --raw-output --arg "key" "purescript-$package_name" '.[$key]' <<< "$registry")"
-      update_bower_dependencies
-      if [ "$org" = purescript-node ]; then
-        update_node_library_changelog "$release_date"
-      elif [ "$org" = purescript-contrib ]; then
-        update_contrib_changelog "$package_name" "$version" "$release_date"
-      elif [ "$package_name" = prelude ]; then
-        update_prelude_changelog "$package_name" "$version" "$release_date"
-      else
-        update_changelog "$package_name" "$version" "$release_date"
-      fi
-      if [ -z "$dry_run" ]; then
-        git push origin "$branch"
-        open_pull_request "$repository_url" "$branch" "$version"
-      fi
-      popd
-      rm -rf "$workspace"
+    local package; package="$(jq --arg key "$package_name" '.[$key]' <<< "$org_packages")"
+    local repository_url; repository_url="$(jq --raw-output '.repo' <<< "$package")"
+    local version; version="$(jq --raw-output '.version' <<< "$package")"
+    local branch; branch="release-$version"
+    local workspace; workspace="$(mktemp --directory)"
+    echo "Preparing to release $package_name@$version"
+    echo "Cloning $repository_url"
+    git clone --depth 1 "$repository_url" "$workspace"
+    pushd "$workspace"
+    if git ls-remote --exit-code --heads origin "$branch" > /dev/null; then
+      echo "Release branch $branch found on remote, skipping $package_name"
+      continue
+    fi
+    git checkout -b "$branch"
+    update_ci "$org"
+    update_bower_repository_url "$(jq --raw-output --arg "key" "purescript-$package_name" '.[$key]' <<< "$registry")"
+    if [ "$org" = purescript-contrib ]; then
+      update_contrib_bower_dependencies "$packages" "$package_name"
+    else
+      update_bower_dependencies "$packages"
+    fi
+    if [ "$org" = purescript-node ]; then
+      update_node_changelog "$release_date"
+    elif [ "$org" = purescript-contrib ]; then
+      update_contrib_changelog "$package_name" "$version" "$release_date"
+    elif [ "$package_name" = prelude ]; then
+      update_prelude_changelog "$package_name" "$version" "$release_date"
+    else
+      update_changelog "$org" "$package_name" "$version" "$release_date"
+    fi
+    if [ -z "$dry_run" ]; then
+      git push --set-upstream origin "$branch"
+      open_pull_request "$org" "$repository_url" "$version"
+    fi
+    popd
+    rm -rf "$workspace"
   done
 }
-
-package_set='https://raw.githubusercontent.com/kl0tl/package-sets/next'
-packages="$(dhall-to-json <<< "$package_set/src/groups/purescript.dhall // $package_set/src/groups/purescript-contrib.dhall // $package_set/src/groups/purescript-web.dhall // $package_set/src/groups/purescript-node.dhall")"
-registry="$(curl --silent --show-error https://raw.githubusercontent.com/purescript/registry/master/bower-packages.json)"
 
 dry_run=''
 if [ "${1-}" = --dry-run ]; then
@@ -171,7 +204,8 @@ if [ "${1-}" = --dry-run ]; then
   shift
 fi
 
-release_date="$(date --date "${1-}" '+%Y-%m-%d')"
+release_date=2021-02-26
+# release_date="$(date --date "${1-}" '+%Y-%m-%d')"
 
 while true; do
   read -rp "Which org should be released${dry_run:+ (this is a dry run)}? " org
