24fc90bec83d0ea5b6e7f961a22c95865c0cbbcc
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-04-06 15:35:15 -0500

    Define initial CLI interface

---
+++ src/Command.purs
@@ -0,0 +1,37 @@
+module Command where
+
+import Prelude
+
+import Data.Maybe (Maybe)
+import Data.Version (Version)
+import Types (GitHubOwner, Package)
+
+data Command
+  -- | Initializes the project structure
+  = Init
+  -- | Clone all packages
+  | CloneAll { makeFork :: Maybe GitHubOwner }
+  -- | Runs all update actions across all repos
+  | UpdateAll
+  -- | Gets a local copy of the purs binary from GitHub
+  | DownloadPurs Version
+  -- | Clone a repo locally with the option of making a fork
+  | Clone { package :: Package, makeFork :: Maybe GitHubOwner }
+  -- | Update bower.json file
+  | Bower { package :: Package }
+  -- | Update spago.dhall and packages.dhall files
+  | Spago { package :: Package }
+  -- | Compile the package
+  | Compile { package :: Package, clearBowerCache :: Boolean }
+  -- | Update package.json file
+  | PackageJson { package :: Package }
+  -- | Update eslintrc.json file
+  | Eslint { package :: Package }
+  -- | Update ci.yml file
+  | CI { package :: Package }
+  -- | Check for any deprecations
+  | Check { package :: Package }
+  -- | Create a PR
+  | MakePr { package :: Package }
+  -- | Show examples
+  | ShowExamples
