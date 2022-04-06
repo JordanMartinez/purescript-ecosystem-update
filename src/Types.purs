f20702cfd91bebeab31b9100386c7deef16ae802
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-04-06 15:32:02 -0500

    Define a few initial types

---
+++ src/Types.purs
@@ -0,0 +1,43 @@
+module Types where
+
+import Prelude
+
+import Data.Newtype (class Newtype)
+
+newtype Package = Package String
+derive newtype instance Eq Package
+derive newtype instance Ord Package
+derive instance Newtype Package _
+derive newtype instance Show Package
+
+newtype GitHubOwner = GitHubOwner String
+derive instance Eq GitHubOwner
+derive instance Ord GitHubOwner
+derive instance Newtype GitHubOwner _
+derive newtype instance Show GitHubOwner
+
+newtype GitHubProject = GitHubProject String
+derive instance Eq GitHubProject
+derive instance Ord GitHubProject
+derive instance Newtype GitHubProject _
+derive newtype instance Show GitHubProject
+
+newtype GitCloneUrl = GitCloneUrl String
+derive instance Eq GitCloneUrl
+derive instance Ord GitCloneUrl
+derive instance Newtype GitCloneUrl _
+derive newtype instance Show GitCloneUrl
+
+newtype BranchName = BranchName String
+derive instance Eq BranchName
+derive instance Ord BranchName
+derive instance Newtype BranchName _
+derive newtype instance Show BranchName
+
+type PackageInfo =
+  { name :: Package
+  , owner :: GitHubOwner
+  , project :: GitHubProject
+  , gitUrl :: GitCloneUrl
+  , defaultBranch :: BranchName
+  }
