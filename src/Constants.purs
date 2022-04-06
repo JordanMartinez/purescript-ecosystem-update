c6aab98622ae98101afea1ed3b853342a5d12ef4
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-04-06 15:31:45 -0500

    Define a few constants

---
+++ src/Constants.purs
@@ -0,0 +1,27 @@
+module Constants where
+
+import Node.Path as Path
+
+ciYmlFile :: String
+ciYmlFile = Path.concat [ ".github", "workflows", "ci.yml" ]
+
+eslintFile :: String
+eslintFile = ".eslintrc.json"
+
+bowerJsonFile :: String
+bowerJsonFile = "bower.json"
+
+packagesJsonFile :: String
+packagesJsonFile = "packages.json"
+
+packagesDhallFile :: String
+packagesDhallFile = "packages.dhall"
+
+spagoDhallFile :: String
+spagoDhallFile = "spago.dhall"
+
+pursJsonFile :: String
+pursJsonFile = "purs.json"
+
+gitIgnoreFile :: String
+gitIgnoreFile = ".gitignore"
