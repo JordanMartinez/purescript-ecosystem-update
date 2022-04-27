910889810e9d7367496afe6d2c8ffcca8c8e0522
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-04-27 15:08:29 -0500

    Override react version to 10.0.1

--- src/DependencyGraph.purs
+++ src/DependencyGraph.purs
@@ -131,4 +131,5 @@ packageVersionOverrides :: Array (Tuple String Version)
 packageVersionOverrides =
   [ Tuple "arraybuffer-types" $ version 3 0 2 Nil Nil
   , Tuple "type-equality" $ version 4 0 1 Nil Nil
+  , Tuple "react" $ version 10 0 1 Nil Nil
   ]
